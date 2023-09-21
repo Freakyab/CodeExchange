5c59b6194377a1346d3aad529e7bdb4875707034
Freakyab <aryanbhisikar1@gmail.com>
2023-09-21 23:16:48 +0530

    feat: clipboard and download

--- src/app/Editor/EditorReadOnly.jsx
+++ src/app/Editor/EditorReadOnly.jsx
@@ -6,7 +6,7 @@ const EditorBlock = ({ data, holder,onChange, share }) => {
   useEffect(() => {
     const sendData = async (content) => {
       try {
-        const res = await fetch("https://code-exchange-backend.vercel.app/create/", {
+        const res = await fetch("http://localhost:5000/create/", {
           method: "POST",
           body: JSON.stringify({
             code: content, // Use a different variable name here to avoid conflicts
@@ -32,7 +32,7 @@ const EditorBlock = ({ data, holder,onChange, share }) => {
         autofocus: true,
         onChange: async (api, event) => {
           const savedData = await api.saver.save();
-          // control.setUpdateCode(savedData);
+          // controls.setSelfText(true);
           sendData(savedData);
         }
       });
--- src/app/share/[share]/page.jsx
+++ src/app/share/[share]/page.jsx
@@ -3,19 +3,22 @@ const EditorBlock = dynamic(() => import("../../Editor/EditorReadOnly"), {
   ssr: false,
 });
 import dynamic from "next/dynamic";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import styles from "../../Home.module.css"; // Import CSS module for local styles
+import { BsFillClipboardFill, BsDownload } from "react-icons/bs";
+import { ToastContainer, toast } from "react-toastify";
+
 import "react-toastify/dist/ReactToastify.css";
 
 const CodePage = ({ params }) => {
   const [code, setCode] = useState("");
-
+  const textInputRef = useRef(null);
   useEffect(() => {
     getData();
   }, []);
 
   const getData = async () => {
-    const res = await fetch("https://code-exchange-backend.vercel.app/get", {
+    const res = await fetch("http://localhost:5000/get", {
       method: "POST",
       body: JSON.stringify({
         share: params.share,
@@ -26,30 +29,68 @@ const CodePage = ({ params }) => {
     });
     const data = await res.json();
     if (data.isSuccess) setCode(data.code);
-    
+  };
+
+  const handleCopy = () => {
+    if (textInputRef.current) {
+      const range = document.createRange();
+      range.selectNodeContents(textInputRef.current);
+      const selection = window.getSelection();
+      selection.removeAllRanges();
+      selection.addRange(range);
+      document.execCommand("copy");
+      toast.success("Copied to clipboard");
+    }
+  };
+  const handleDownload = () => {
+    if (textInputRef.current) {
+      const range = document.createRange();
+      range.selectNodeContents(textInputRef.current);
+      const selection = window.getSelection();
+      selection.removeAllRanges();
+      selection.addRange(range);
+      document.execCommand("copy");
+      const selectedText = selection.toString();
+      const blob = new Blob([selectedText], { type: "text/plain" });
+
+      // Create a temporary URL and trigger the download
+      const url = window.URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = "selected_text.txt"; // Set the filename
+      document.body.appendChild(a);
+      a.click();
+
+      // Clean up
+      window.URL.revokeObjectURL(url);
+      document.body.removeChild(a);
+    }
   };
 
   return (
     <div className={styles.fancyBackground}>
-      <div className="bg-white rounded-lg shadow-2xl m-4 md:w-3/5 lg:w-[50vw] xl:w-[60vw]">
+      <div className="bg-white rounded-lg shadow-2xl m-4 w-[70vw]">
         <div className="bg-gray-700 text-white py-4 px-6 rounded-t-lg flex justify-between">
-          <h1 className="text-xl font-semibold">Blogger's Editor</h1>
-          <button
-            className="text-xl text-black bg-white font-semibold cursor-pointer hover:bg-gray-300 px-4 rounded-lg">
-            HELLO,
-          </button>
+          <h1 className="text-xl font-semibold">CodeExchange</h1>
+          <div>
+            <button onClick={handleCopy} className="px-4">
+              <BsFillClipboardFill size={20} />
+            </button>
+            <button onClick={handleDownload}>
+              <BsDownload size={20} />
+            </button>
+          </div>
         </div>
-        <div>
+        <div ref={textInputRef}>
           <EditorBlock
             data={code}
-            
             onChange={setCode}
             holder="editorjs-container"
             share={params.share}
           />
         </div>
       </div>
-     
+      <ToastContainer />
     </div>
   );
 };
