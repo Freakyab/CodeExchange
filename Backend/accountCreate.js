06f510b89c3f87ba8976ca0e884ab3b61bd9c478
Freakyab <aryanbhisikar1@gmail.com>
2023-09-26 00:08:26 +0530

    feat :backend new files added

---
+++ Backend/accountCreate.js
@@ -0,0 +1,58 @@
+const { MongoClient } = require("mongodb");
+const express = require("express");
+const router = express.Router();
+const uri =
+  "mongodb+srv://DynamicA:D0j3iO5c23I9Lmbo@cluster0.wxp0mkv.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri);
+
+router.post("/", async (req, res) => {
+  const { code, account,_id } = req.body;
+  try {
+    await client.connect();
+    const database = client.db("codeExchange");
+    const collection = database.collection("premium");
+    let result;
+
+    const findBlock = await collection.findOne({ account: account });
+    
+    if (findBlock) {
+       result = await collection.updateOne(
+        { account: account },
+        {
+          $set: {
+            code: code,
+          },
+        }
+      );
+    } else {
+       result = await collection.insertOne({
+        code: code,
+        account: account,
+        owner : _id
+      });
+    }    
+    
+   // Instead of setTimeout, consider using a background worker or job queue for more reliability.
+setTimeout(async () => {
+  try {
+    const updatedDocument = await collection.findOne({account:account});
+    if (updatedDocument?.code?.blocks?.length == 0) {
+      await collection.deleteOne({ account:account });
+    }
+  } catch (error) {
+    console.error("Error during deletion:", error);
+  }
+}, 2000);
+
+
+
+    if (result) res.json({ isSuccess: true });
+    else res.json({ isSuccess: false });
+  } catch (e) {
+    console.error(e);
+    res.json({ isSuccess: false });
+  }
+});
+
+module.exports = router;
---
+++ Backend/signup.js
@@ -0,0 +1,30 @@
+const { MongoClient } = require('mongodb');
+const express = require('express');
+const router = express.Router();
+
+const client = new MongoClient('mongodb+srv://DynamicA:D0j3iO5c23I9Lmbo@cluster0.wxp0mkv.mongodb.net/?retryWrites=true&w=majority',);
+
+router.post('/', async (req, res) => {
+    try {
+        const { username, password, name } = req.body;
+        if (username === "" || password === "" || name === "") {
+            res.json({ success: false });
+        }
+
+        await client.connect();
+        const db = client.db('codeExchange');
+        const collection = db.collection('accounts');
+        const user = await collection.insertOne({ username, password, name });
+
+        if (user) {
+            res.json({ success: true, id: user.insertedId, name: name });
+        } else {
+            res.json({ success: false });
+        }
+    } catch (e) {
+        console.log(e);
+        res.json({ success: false });
+    }
+});
+
+module.exports = router;
