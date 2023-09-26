a4b038b566ff47cd348d0513230fe591dbfe34f4
Freakyab <aryanbhisikar1@gmail.com>
2023-09-27 00:29:01 +0530

    testing nextauth url

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -48,6 +48,7 @@ const credentials = CredentialsProvider({
 const google = GoogleProvider({
   clientId: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  redirectUri: process.env.NEXTAUTH_URL, 
 });
 
 const github = GithubProvider({
