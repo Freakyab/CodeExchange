0641ade1e3ba93dd5328a34e2aa01680ac459b87
Freakyab <aryanbhisikar1@gmail.com>
2023-09-27 00:11:33 +0530

    testing

--- src/app/page.js
+++ src/app/page.js
@@ -39,7 +39,7 @@ export default function Home() {
         <p className="w-full overflow-x-auto text-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-4 pt-6 md:pb-6 md:pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by adding a keyword to{" "}
           <code className="font-mono font-bold ">
-            "https://codeexchange.netlify.app/share/yourkeyword"
+            "https://codeexchange.vercel.app/share/yourkeyword"
           </code>{" "}
           in the URL
         </p>
